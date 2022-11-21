b691c733c93281dc98bffd0aee6809437469aa28
Sergey Pichugin <rutilus89@mail.ru>
2022-11-21 13:10:57 +0300

    Dodelal privyazku Roley!2

--- src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -54,4 +54,10 @@ public class Role implements GrantedAuthority {
     public String getAuthority() {
         return getName();
     }
+
+    @Override
+    public String toString() {
+        return  name.substring(5);
+
+    }
 }
